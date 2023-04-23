d82a3e56bf0a7fd14e2757d707d0eaa7b5629c75
Mateusj2506 <mateus_jorge2002@outlook.com>
2023-04-23 16:33:43 -0300

    Tela esqueci senha

--- costs/src/componentes/paginas/TelaInicio.js
+++
@@ -1,6 +0,0 @@
-function Menu() {
-    return <h1>Java</h1>
-}
-
-export default Menu
-
--- src/componentes/paginas/Alterar_sen_email.js
+++ src/componentes/paginas/Alterar_sen_email.js
@@ -1,5 +1,55 @@
+import React from "react";
+import imgCadeado from "../../img/Imagem de cadeado.png"
+import './Alterar_sen_email.css';
+import imgSeparacao from "../../img/Separação.png"
+
 function Alterar_sen() {
-    return <h1>Java</h1>
+    
+    const handleSumit = (event) => {
+        event.preventDefault();
+    }
+    
+    return (
+        <div className="Conteiner">
+        <form  className="FormEsqueceuSenha" onSubmit={handleSumit}>
+
+        <div className='conteinerImg'>
+            <img src={imgCadeado} width={100}></img>
+        </div>
+
+        <div className="conteinerTitle">
+            <h1>Problemas para entrar?</h1>
+        </div>
+
+        <div className="conteinerP">
+            <h5>Insira seu email e enviaremos um </h5>
+        </div>
+
+        <div className="conteinerP1">
+            <h5>link para redefinir sua senha.</h5>
+        </div>
+
+        <div className="conteinerBox">
+            <input type='email' className='inptEmailPassword' name='email' placeholder='Insira seu email' />
+        </div>
+
+        <div className="conteinerBTN">
+            <button className='button' type='submit'>Entrar</button>
+        </div>
+
+        <div className="conteinerDivisoria">
+            <img src={imgSeparacao} width={100} ></img>
+            <p>Ou</p>
+            <img src={imgSeparacao} width={100} ></img>
+        </div>
+
+        <div className="conteinerLink">
+            <a className='conteinerLink' href='Cadastro.js' target='_black'> Criar nova conta</a>
+        </div>
+
+        </form>
+        </div>
+    );
 }
 
 export default Alterar_sen
