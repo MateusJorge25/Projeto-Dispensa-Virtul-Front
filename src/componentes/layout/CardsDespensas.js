801423583943db9076ef36fcce9485eb3e1080ac
WAGNER SOUZA <wagner.souza20@fatec.sp.gov.br>
2023-05-19 15:27:42 -0300

    listar cards despensas

--- src/componentes/layout/CardsDespensas.js
+++ src/componentes/layout/CardsDespensas.js
@@ -2,19 +2,19 @@ import "../layout/CardDespensa.css";
 import Geladeira from "../../img/iconGeladeira.png";
 import Lavanderia from "../../img/lavanderia.png";
 import ArmarioCozinha from "../../img/armarioCozinha.png";
-
+import caixaDeLeite from "../../img/caixaDeLeite.png";
 
 const CardsDespesas = ({img ,nomeProduto}) => {
 //como vamos receber as imagens atravez da string acredito que vamos precisa fazer uma validação semelhante a esta:
-    // let imagem;
-    // if (img == "caixaDeLeite"){
-    //     imagem = caixaDeLeite;
-    // }
+    let imagem;
+    if (img == "MaquinaDeLavar"){
+        imagem = caixaDeLeite;
+    }
 
     return (
         <div className="ContainerDespesas">
             <div className="nomeImagemDespensa">
-                <img className="imagemDoCardCategoria" src={img} width={70} />
+                <img className="imagemDoCardCategoria" src={imagem} width={70} />
                 <h1 className="nomeCategoria">{nomeProduto}</h1> 
             </div>
         </div>
--- src/componentes/paginas/Despesas/Despesa.js
+++ src/componentes/paginas/Despesas/Despesa.js
@@ -2,6 +2,7 @@ import Navbar from "../../layout/NavBar";
 import Container from "../../layout/Container";
 import { Link, useParams } from "react-router-dom";
 import "./Despesa.css";
+import { useEffect, useState } from "react";
 import CardDespesas from "../../layout/CardsDespensas";
 import btnVoltar from "../../../img/Botão de voltar.png"
 import Geladeira from "../../../img/iconGeladeira.png";
@@ -11,21 +12,25 @@ import ArmarioCozinha from "../../../img/armarioCozinha.png";
 const Despesa = () => {
 
     const{ id } = useParams();
+    const [Data, setData] = useState([]);
 
-    const sendApi = async (id) =>{
-        const date  = await fetch(`http://localhost:3000/local/${id}`,{
-            method:"GET",
-            headers:{
-                "Authorization": sessionStorage.getItem("token")
-            }
-        })
+    useEffect(()=>{
+        const sendApi = async (id) =>{
+            const date  = await fetch(`http://localhost:3000/local/${id}`,{
+                method:"GET",
+                headers:{
+                    "Authorization": sessionStorage.getItem("token")
+                }
+            })
+            const apijson = await date.json();
+            setData(apijson);
+            console.log(apijson);
+            return apijson;
+        };
+        sendApi(id);
+    },[])
 
-        const apijson = await date.json();
-        console.log(apijson);
-        return apijson;
-    }
-
-    const elementos = sendApi(id);
+    
 
     return (
         <div className="ContainerDespensaTela">
@@ -34,11 +39,14 @@ const Despesa = () => {
                 <Link to={`/home/${id}`}><img src={btnVoltar} width={35}/></Link>
                 <h1>Despensa</h1>
             </div>
-
             <Container containerMod='TelaProdutos'>  
-                <CardDespesas img={"img"} nomeProduto={"nome"} />
+            {
+                Data.map((Element) => {
+                    return <CardDespesas img={Element.img} nomeProduto={Element.nome}/>
+                })
+            }
+                
             </Container>
-
         </div>
     )
 }
